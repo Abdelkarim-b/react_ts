07e9df3114e9527b304a32d40c32ab0cfa8b1ab2
abdelkarim <abdelkarimbenchelef.1361@gmail.com>
2023-03-03 22:42:38 +0100

    import  PropsType from 'props-types' and use it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import { Person } from './Person';
 
 function App() {
   return (
     <div className="App">
-     
+       <Person 
+          name="abdelkarim"
+          email="abdelkarim@gmail.com"
+          age="28"
+          isMarried={false}
+          friends={["mohamed", "billel"]}
+       />
     </div>
   );
 }
